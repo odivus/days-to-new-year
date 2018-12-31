6733d6da9a52b8791bf531c5669d7b86356382cb
odivus <odivus@gmail.com>
2018-12-31 15:43:25 +0300

    Исправления с датами

--- js/show-days-for-new-year.js
+++ js/show-days-for-new-year.js
@@ -6,24 +6,26 @@ function getDaysForNewYear() {
 
   newYear.setMonth(11); // Установить месяц Декабрь
   newYear.setDate(31); // Установить число
+  //console.log(newYear.getTime());
+  //console.log(today.getTime());
+
+  if (newYear.getTime() === today.getTime()) return null;
 
   if (today.getTime() < newYear.getTime()) {
     difference = newYear.getTime() - today.getTime(); // результат в миллисекундах
     difference = Math.floor(difference / (1000 * 60 * 60 * 24));
+    return difference;
   }
-  return difference;
 };
 
 function showDaysForNewYear() {
-  let today = new Date(),
-    date = today.getDate(),
-    month = today.getMonth(),
-    textDomElement = document.querySelector('.info h1');
+  let textDomElement = document.querySelector('.info h1');
+  let dateResult = getDaysForNewYear();
 
-  if (date === 31 && month === 11) {
+  if (!dateResult) {
     textDomElement.innerHTML = '<span class="days">С новым годом!</span>';  
   } else {
-    textDomElement.innerText = getDaysForNewYear();    
+    textDomElement.innerText = dateResult;    
   }
 }
 
